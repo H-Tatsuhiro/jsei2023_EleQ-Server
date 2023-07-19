994a03faa69f0a3794f6f879bc2af3e8953ba0fa
H.Tatsuhiro <admin@h-tatsu.com>
2023-07-19 22:51:49 +0900

    Updated.

--- QRreader.py
+++ QRreader.py
@@ -2,8 +2,6 @@ import cv2
 
 img = cv2.imread('/Users/h.tatsuhiro/PycharmProjects/jsei2023_EleQ-Server/test.png')
 
-print(img.size)
-
 qrcd = cv2.QRCodeDetector()
 
 retval = qrcd.detectAndDecodeMulti(img)
